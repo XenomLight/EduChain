f4a8602483c3d1c13d7bfd1ee16cc2d4ee1db211
sandy <saputramuhamadsndy@gmail.com>
2025-07-16 10:36:38 +0700

    update webpack

---
+++ webpack.config.cjs
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+    },
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+      { test: /\.svg$/, use: ['@svgr/webpack'] },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.(png|jpg|gif)$/, use: ['file-loader'] },
+    ],
+  },
+  devServer: {
+    static: './dist',
+    port: 3000,
+  },
+};
